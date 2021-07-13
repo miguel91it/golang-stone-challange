0190938e1300433dd94ec2edf4f7ae4458fd7fdd
Joao <joao.miguel@hvarconsulting.com.br>
2021-07-12 23:37:18 -0300

    finalizando a versao inicial dos metodos getAccountBalance e CreateAccount. Passara por refatoraçao.

--- src/main.go
+++ src/main.go
@@ -21,7 +21,14 @@ type Account struct {
 
 type Accounts []Account
 
+var accounts Accounts
+
 func main() {
+
+	accounts = Accounts{
+		Account{Id: 1, Name: "miguel", Cpf: "398.291.098-60", Secret: "passwd", Balance: 1.5, Created_at: time.Now()},
+		Account{Id: 2, Name: "pedro", Cpf: "398.291.098-60", Secret: "passwd", Balance: 1.5, Created_at: time.Now()},
+	}
 
 	router := mux.NewRouter()
 
@@ -36,13 +43,11 @@ func main() {
 
 func GetAccounts(w http.ResponseWriter, r *http.Request) {
 
-	accounts := Accounts{
-		Account{Id: 1, Name: "miguel", Cpf: "398.291.098-60", Secret: "passwd", Balance: 1.5, Created_at: time.Now()},
-	}
-
-	fmt.Print(accounts)
+	// fmt.Print(accounts)
 	w.Header().Set("Content-Type", "application/json")
+
 	w.WriteHeader(http.StatusCreated)
+
 	if err := json.NewEncoder(w).Encode(accounts); err != nil {
 		panic(err)
 	}
@@ -50,8 +55,32 @@ func GetAccounts(w http.ResponseWriter, r *http.Request) {
 
 func GetAccountBalance(w http.ResponseWriter, r *http.Request) {
 
+	params := mux.Vars(r)
+
+	fmt.Println(params)
+	fmt.Println(params["id"])
+
+	fmt.Println(r)
+
+	for _, account := range accounts {
+
+		fmt.Println(account)
+
+		if string(account.Id) == params["id"] {
+			json.NewEncoder(w).Encode(account)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Account{})
+
 }
 
 func CreateAccount(w http.ResponseWriter, r *http.Request) {
+
+	var newAccount Account
+
+	json.NewDecoder(r.Body).Decode(&newAccount)
+
+	accounts = append(accounts, newAccount)
 
 }
