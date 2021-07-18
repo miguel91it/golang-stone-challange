38528380ba267bb3a19c8b47239e78aeb2f77c62
Joao <joao.miguel@hvarconsulting.com.br>
2021-07-18 00:32:41 -0300

    main.go: refatorada para conter uma seçao init() para instanciar o banco de dados em memoria, inserir alguns registros iniciais para testes e, na funçao main, refatoraçao para conter apenas as HandleFunc para tratar as requisiçoes. Todas as funçoes chamadas pela handleFuncs foram movidas para o arquivo handlers.go

--- src/main.go
+++ src/main.go
@@ -1,86 +1,42 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
-	"time"
 
 	mux "github.com/gorilla/mux"
 )
 
-type Account struct {
-	Id         int       `json:"id"`
-	Name       string    `json:"name,omitempty"`
-	Cpf        string    `json:"cpf,omitempty"`
-	Secret     string    `json:"secret,omitempty"`
-	Balance    float64   `json:"balance,omitempty"`
-	Created_at time.Time `json:"created_at,omitempty"`
+var db Storage
+
+func init() {
+
+	// inicia o repositorioinmemory
+	db = NewStorage()
+
+	accounts := Accounts{
+		Account{Name: "miguel", Cpf: "398.291.098-60", Secret: "passwd", Balance: 1.5},
+		Account{Name: "pedro", Cpf: "123.456.098-60", Secret: "passwd", Balance: 1.5},
+	}
+
+	if err := db.SaveAccount(accounts...); err != nil {
+		panic(err.Error())
+	}
+
 }
 
-type Accounts []Account
-
-var accounts Accounts
-
 func main() {
-
-	accounts = Accounts{
-		Account{Id: 1, Name: "miguel", Cpf: "398.291.098-60", Secret: "passwd", Balance: 1.5, Created_at: time.Now()},
-		Account{Id: 2, Name: "pedro", Cpf: "398.291.098-60", Secret: "passwd", Balance: 1.5, Created_at: time.Now()},
-	}
 
 	router := mux.NewRouter()
 
 	router.HandleFunc("/accounts", GetAccounts).Methods("GET")
-
 	router.HandleFunc("/accounts/{id}/balance", GetAccountBalance).Methods("GET")
-
 	router.HandleFunc("/accounts", CreateAccount).Methods("POST")
 
+	router.HandleFunc("/transfer", GetTransfers).Methods("GET")
+	router.HandleFunc("/transfer", MakeTransfer).Methods("POST")
+
+	// router.HandleFunc("/login", Login).Methods("POST")
+
 	log.Fatal(http.ListenAndServe(":8000", router))
-}
-
-func GetAccounts(w http.ResponseWriter, r *http.Request) {
-
-	// fmt.Print(accounts)
-	w.Header().Set("Content-Type", "application/json")
-
-	w.WriteHeader(http.StatusCreated)
-
-	if err := json.NewEncoder(w).Encode(accounts); err != nil {
-		panic(err)
-	}
-}
-
-func GetAccountBalance(w http.ResponseWriter, r *http.Request) {
-
-	params := mux.Vars(r)
-
-	fmt.Println(params)
-	fmt.Println(params["id"])
-
-	fmt.Println(r)
-
-	for _, account := range accounts {
-
-		fmt.Println(account)
-
-		if string(account.Id) == params["id"] {
-			json.NewEncoder(w).Encode(account)
-			return
-		}
-	}
-	json.NewEncoder(w).Encode(&Account{})
-
-}
-
-func CreateAccount(w http.ResponseWriter, r *http.Request) {
-
-	var newAccount Account
-
-	json.NewDecoder(r.Body).Decode(&newAccount)
-
-	accounts = append(accounts, newAccount)
-
 }
