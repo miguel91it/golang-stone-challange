631be3efc775235b0fb94d993c1d59a48029f359
Joao <joao.miguel@hvarconsulting.com.br>
2021-07-18 00:35:09 -0300

    account.go: criado o arquivo account.go para conter a estrutura de Account e seus metodos.

---
+++ src/account.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"time"
+)
+
+type Account struct {
+	Id         int       `json:"id"`
+	Name       string    `json:"name,omitempty"`
+	Cpf        string    `json:"cpf,omitempty"`
+	Secret     string    `json:"secret,omitempty"`
+	Balance    float64   `json:"balance"`
+	Created_at time.Time `json:"created_at,omitempty"`
+}
+
+type Accounts []Account
+
+func (a *Account) UpdateBalance(ammount float64) (float64, error) {
+	// func para atualizar o balance cumulativamente
+	a.Balance = a.Balance + ammount
+
+	return a.Balance, nil
+}
+
+func (a *Account) CheckBalanceForDebit(ammountToDebit float64) (bool, error) {
+
+	currentBalance := a.Balance
+
+	if ammountToDebit > currentBalance {
+
+		return false, fmt.Errorf("this operation cannot be performed. Current Account Balance '%f' is less than the Ammount to debit %f", currentBalance, ammountToDebit)
+	}
+
+	return true, nil
+}
+
+func (a *Account) HashSecret() {
+
+	h := sha256.New()
+
+	h.Write([]byte(a.Secret))
+
+	secret_hash := h.Sum(nil)
+
+	a.Secret = hex.EncodeToString(secret_hash)
+}
